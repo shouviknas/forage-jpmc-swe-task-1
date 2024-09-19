53fdb9f615aad55ace505aa5ff8a476cdef1aecf
Shouvik <shouvik7468845@gmail.com>
2024-09-19 18:53:30 +0530

    new update on Client_test file

--- client_test.py
+++ client_test.py
@@ -8,6 +8,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(
+        getDataPoint(quote),
+        (
+            quote['stock'],
+            float(quote['top_bid']['price']),
+            float(quote['top_ask']['price']),
+            (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+        )
+    )
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +25,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+for quote in quotes:
+    self.assertEqual(
+        getDataPoint(quote),
+        (
+            quote['stock'],
+            float(quote['top_bid']['price']),
+            float(quote['top_ask']['price']),
+            (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+        )
+    )
 
 
   """ ------------ Add more unit tests ------------ """
